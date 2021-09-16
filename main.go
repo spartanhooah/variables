403869b385dd8794490902764cede2eb14e7b63b
ben <spartanhooah@gmail.com>
2021-09-16 09:24:18 -0400

    refactor, add randomness

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"time"
 )
 
 const prompt = "and press ENTER when ready."
 
 func main() {
-	var firstNumber = rand.Intn(8)
-	var secondNumber = 5
-	var subraction = 7
+	rand.Seed(time.Now().UnixNano())
+	var firstNumber = random1to10()
+	var secondNumber = random1to10()
+	var subtraction = random1to10()
+	answer := firstNumber*secondNumber - subtraction
 
+	runTheGame(firstNumber, secondNumber, subtraction, answer)
+}
+
+func random1to10() int {
+	return rand.Intn(8) + 2
+}
+
+func runTheGame(firstNumber, secondNumber, subtraction, answer int) {
 	reader := bufio.NewReader(os.Stdin)
 
 	// display a welcome message with instructions
@@ -32,10 +43,9 @@ func main() {
 	fmt.Println("Divide the result by the number you originally thought of", prompt)
 	reader.ReadString('\n')
 
-	fmt.Println("Now subtract", subraction, prompt)
+	fmt.Println("Now subtract", subtraction, prompt)
 	reader.ReadString('\n')
 
 	// give the answer
-	answer := firstNumber * secondNumber - subraction
 	fmt.Println("The answer is", answer)
 }
